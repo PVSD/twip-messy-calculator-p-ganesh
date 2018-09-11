b3486ecb442e091eaedbddc94be4e07ffaae34c4
p-ganesh <pg304@pvsd.org>
2018-09-11 11:16:46 -0400

    shoot from the hip; keep it real

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kbreader = new Scanner(System.in);
+
+        System.out.println("Would you like to add, subtract, multiply, or divide?");
+        String command = kbreader.nextLine();
+
+        System.out.println("Please enter your first input.");
+        int numberOne = kbreader.nextInt();
+
+        System.out.println("Please enter your second input.");
+        int numberTwo = kbreader.nextInt();
+
+        if (command = = "add") {
+            int solution = Math.ceil(numberOne + numberTwo);
+            System.out.println("The sum of " + numberOne + " and " + numberTwo " is " + solution + ". Are you satisfied?");
+
+            String satisfy = kbreader.nextLine();
+
+            if (satisfy.length() == 2){
+                System.out.println("Can you identify what math class method I ran the real answer through?");
+
+
+            }
+        }
     }
 }
