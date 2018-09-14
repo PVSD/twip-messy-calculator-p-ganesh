32f8e38b79eefbca354b76cc2066ad7b02e4604f
p-ganesh <41019754+p-ganesh@users.noreply.github.com>
2018-09-13 21:28:58 -0400

    weed is the devil's lettuce

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,26 +7,98 @@ public class Main {
     public static void main(String[] args) {
         Scanner kbreader = new Scanner(System.in);
 
-        System.out.println("Would you like to add, subtract, multiply, or divide?");
-        String command = kbreader.nextLine();
-
         System.out.println("Please enter your first input.");
         int numberOne = kbreader.nextInt();
 
         System.out.println("Please enter your second input.");
         int numberTwo = kbreader.nextInt();
 
-        if (command = = "add") {
-            int solution = Math.ceil(numberOne + numberTwo);
-            System.out.println("The sum of " + numberOne + " and " + numberTwo " is " + solution + ". Are you satisfied?");
+        System.out.println("Would you like to add, subtract, multiply, or divide?");
+        String command = kbreader.next();
 
-            String satisfy = kbreader.nextLine();
+        if (command.equalsIgnoreCase("add")) {
+            double solution = Math.cos(numberOne + numberTwo);
+            System.out.println("The sum of " + numberOne + " and " + numberTwo + " is " + solution + ". Are you satisfied?");
 
-            if (satisfy.length() == 2){
+            String satisfy = kbreader.next();
+
+            if (satisfy.equalsIgnoreCase("no")){
                 System.out.println("Can you identify what math class method I ran the real answer through?");
+                String method = kbreader.next();
 
+                if (method.equals("cos")) {
+                    System.out.println("Great! Good job and goodbye!");
 
+                }
+                else {
+                System.out.println("Oops! Better luck next time! Goodbye!");
+                    }}
+            else {
+                    System.out.println("Great! Good job and goodbye!");
+                }
+                }
+        else if (command.equalsIgnoreCase("subtract")){
+            double solution = Math.sqrt(numberOne - numberTwo);
+            System.out.println("The difference of " + numberOne + " and " + numberTwo + " is " + solution + ". Are you satisfied?");
+
+            String satisfy = kbreader.next();
+
+            if (satisfy.equalsIgnoreCase("no")){
+                System.out.println("Can you identify what math class method I ran the real answer through?");
+                String method = kbreader.next();
+
+                if (method.equals("sqrt")) {
+                    System.out.println("Great! Good job and goodbye!");
+
+                }
+                else {
+                    System.out.println("Oops! Better luck next time! Goodbye!");
+                }}
+            else {
+                System.out.println("Great! Good job and goodbye!");
             }
         }
-    }
-}
+        else if (command.equalsIgnoreCase("multiply")) {
+            double solution = Math.sin(numberOne * numberTwo);
+            System.out.println("The product of " + numberOne + " and " + numberTwo + " is " + solution + ". Are you satisfied?");
+
+            String satisfy = kbreader.next();
+
+            if (satisfy.equalsIgnoreCase("no")){
+                System.out.println("Can you identify what math class method I ran the real answer through?");
+                String method = kbreader.next();
+
+                if (method.equals("sin")) {
+                    System.out.println("Great! Good job and goodbye!");
+
+                }
+                else {
+                    System.out.println("Oops! Better luck next time! Goodbye!");
+                }}
+            else {
+                System.out.println("Great! Good job and goodbye!");
+            }
+        }
+        else if (command.equalsIgnoreCase("divide")) {
+            double solution = Math.floor(numberOne / numberTwo);
+            System.out.println("The quotient of " + numberOne + " and " + numberTwo + " is " + solution + ". Are you satisfied?");
+
+            String satisfy = kbreader.next();
+
+            if (satisfy.equalsIgnoreCase("no")){
+                System.out.println("Can you identify what math class method I ran the real answer through?");
+                String method = kbreader.next();
+
+                if (method.equals("floor")) {
+                    System.out.println("Great! Good job and goodbye!");
+
+                }
+                else {
+                    System.out.println("Oops! Better luck next time! Goodbye!");
+                }}
+            else {
+                System.out.println("Great! Good job and goodbye!");
+            }
+        }
+        }
+        }
